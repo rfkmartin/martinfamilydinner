04b834264be19398108c5148659d1032acae1bf0
Rob Martin <rfkmartin@gmail.com>
2016-06-06 06:22:56 -0500

    logging

---
+++ logger.php
@@ -0,0 +1,35 @@
+<?php
+
+$OUTFILE="/var/log/mfd.log";
+
+function logger($link,$session,$user,$message)
+{
+   echo ' before logger insert ';
+   echo '$session: '.$session;
+   echo '$user: '.$user;
+   echo '$sql: '.mysqli_real_escape_string($link,$message);
+   $sql = "insert into logger (msg_dt,user_id,session_id,message) values (now(),\"".$session."\",\"".$user."\",\"".mysqli_real_escape_string($link,$message)."\")";
+   if (mysqli_query($link,$sql))
+   {
+      echo ' after logger insert ';
+   }
+   else
+   {
+         echo "Error inserting logger: " . mysqli_error($link);
+   }
+
+   // Format the date and time
+   $date = date("Y-m-d H:i:s", $time);
+
+   if ($fd = @fopen($OUTFILE, "a"))
+   {
+      $result = fputcsv($fd, array($date, $session, $user, $sqlstring));
+      fclose($fd);
+   }
+   else
+   {
+   echo ' had trouble opening file ';
+   }
+}
+
+?>
